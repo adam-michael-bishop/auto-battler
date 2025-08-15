67cf53033efac14a53be88ec439f093d581a3a3b
adam-michael-bishop <adam.michael.bishop@gmail.com>
2025-08-15 13:13:24 -0500

    Add CooldownComponent class to manage cooldown durations for abilities

---
+++ Scripts/Stats/CooldownComponent.cs
@@ -0,0 +1,10 @@
+using System;
+using Godot;
+
+namespace AutoBattler.Stats
+{
+    public partial class CooldownComponent : Resource
+    {
+        public float CooldownLength { get; set; }
+    }
+}
